b6f147d6cdbb4b48aedb2a904f3363ff53d877dc
Belmokhtar <Mohamed@crista05.univ-lr.fr>
2016-04-05 17:00:53 +0200

    version 2
    suppression du logo natif
    leader entièrement en html (responsive)
    changement du footer

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+mvn install
+scp ./target/templateProject-1.0-SNAPSHOT.jar root@docdev:/Doc/apps/ulr-config-nuxeo-71/templates/ulr-custom/bundles/
--- src/main/java/org/nuxeo/themeulr/App.java
+++ src/main/java/org/nuxeo/themeulr/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Theme ULR is Deployed!" );
     }
 }
